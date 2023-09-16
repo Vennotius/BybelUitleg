b5c63e90e6f173a7fcf9413b6c2d8e043961ff2b
Vennotius <mcadendorff@gmail.com>
2023-09-16 15:57:33 +0200

    noiceness

--- bybel_uitleg.py
+++ bybel_uitleg.py
@@ -72,28 +72,54 @@ BYBEL_BOEKNAME = [
 
 BYBEL_TEKS = []
 
-with open('Bybel.json', 'r') as f:
+with open('Bybel.json', 'r', encoding='utf-8') as f:
     BYBEL_TEKS = json.loads(f.read())
 
 
-from pylatex import Document, Command, NewPage, NewLine, UnsafeCommand
+from pylatex import Document, NewPage, Command, Package, NewLine, UnsafeCommand
 from pylatex.utils import NoEscape
-from pylatex.package import Package
 
 doc = Document()
 
+doc.preamble.append(Package('helvet'))
+# doc.preamble.append(Package('garamondx'))
+doc.preamble.append(Package('mathpazo'))
+
+# Add geometry package with custom margin settings
+doc.preamble.append(Package('geometry', options=['a4paper', 'top=3cm', 'bottom=3cm', 'left=2cm', 'right=2cm']))
+
 # Add the utf8x option for inputenc
 #doc.preamble.append(Package('inputenc', options='utf8x'))
+
+# Activate the Helvetica (Arial-like) font
+# doc.append(NoEscape(r'\renewcommand{\rmdefault}{phv}'))  # Set Helvetica as the default text font
+# doc.append(NoEscape(r'\renewcommand{\sfdefault}{phv}'))  # Set Helvetica as the default sans-serif font
+# doc.append(NoEscape(r'\renewcommand{\rmdefault}{zgmr}'))  # Set Garamond as the default text font
+# Add garamondx package for Garamond font
+
+# Activate the Palatino font
+doc.append(NoEscape(r'\renewcommand{\rmdefault}{ppl}'))  # Set Palatino as the default text font
 
 
 # Add packages for multicol and fancyhdr
 doc.preamble.append(Package('multicol'))
 doc.preamble.append(Package('fancyhdr'))
 
+# Set custom spacing between columns
+doc.append(NoEscape(r'\setlength{\columnsep}{0.8cm}'))
+
+# Redefine what 'normalsize' means
+doc.append(NoEscape(r'\renewcommand{\normalsize}{\fontsize{12}{18}\selectfont}'))
+
+# Left align the text and disable hyphenation
+doc.append(NoEscape(r'\raggedright'))
+doc.append(NoEscape(r'\hyphenpenalty=10000'))
+doc.append(NoEscape(r'\exhyphenpenalty=10000'))
+
 # Set custom header and footer
 doc.preamble.append(NoEscape(r'\pagestyle{fancy}'))
 doc.preamble.append(NoEscape(r'\fancyhf{}'))  # Clear existing header and footer
-doc.preamble.append(NoEscape(r'\fancyhead[C]{\textbf{\rightmark}}'))  # Book name and chapter in header
+doc.preamble.append(NoEscape(r'\fancyhead[C]{\fontsize{18}{20}\selectfont\textbf{\rightmark}}'))  # Larger book name and chapter in header
 doc.preamble.append(NoEscape(r'\fancyfoot[C]{\thepage}'))  # Page number in footer
 
 # Title logic as before
@@ -103,21 +129,33 @@ doc.append(NoEscape(r'\maketitle'))
 
 
 # from unidecode import unidecode
+doc.preamble.append(Package('lettrine'))
 
+doc.append(NoEscape(r'\begin{multicols}{2}'))  # Start 2-column layout
 for book_index, book in enumerate(BYBEL_TEKS):
     if book_index > 0:
         break
     for chapter_index, chapter in enumerate(book):
-        doc.append(NoEscape(r'\begin{multicols}{2}'))  # Start 2-column layout
-        doc.append(NoEscape(f'\\renewcommand{{\\rightmark}}{{{BYBEL_BOEKNAME[book_index]} Chapter {chapter_index + 1}}}'))  # Set header
-        doc.append(NoEscape(f'\\noindent\\fontsize{{24}}{{24}}\\selectfont\\textbf{{{chapter_index}}}\\normalsize'))
+        doc.append(NoEscape(f'\\renewcommand{{\\rightmark}}{{{BYBEL_BOEKNAME[book_index]} {chapter_index + 1}}}'))  # Set header
+        
+        # Reduce vertical space before the lettrine
+        doc.append(NoEscape(f'\\vspace*{{-0.5em}}'))
+        
+        # doc.append(NoEscape(f'\\noindent\\fontsize{{16}}{{16}}\\selectfont\\underline{{\\textbf{{{chapter_index}}}}}\\textbf{{ }}\\normalsize'))
+        # doc.append(NoEscape(f'\\begin{{center}}\\fontsize{{24}}{{24}}\\selectfont\\textbf{{{chapter_index}}}\\normalsize\\end{{center}}'))
+        # doc.append(NoEscape(f'\\noindent\\rule{{\\textwidth}}{{1pt}}\\begin{{center}}\\fontsize{{24}}{{24}}\\selectfont\\textbf{{Chapter {chapter_index}}}\\normalsize\\end{{center}}\\noindent\\rule{{\\textwidth}}{{1pt}}'))
+        doc.append(NoEscape(f'\\lettrine[lines=2, lraise=0.1, findent=0.5em, nindent=0em]{{\\textbf{{{chapter_index}}}}}{{}}\\normalsize'))
+
         for verse_index, verse in enumerate(chapter):
+            verse = verse.strip()
             if verse_index > 0:
-                doc.append(NoEscape(f'\\textsuperscript{{\\textbf{{{verse_index + 1}}}}} {verse}'))
+               doc.append(NoEscape(f"\\textsuperscript{{\\fontsize{{9}}{{9}}\\selectfont\\textbf{{ {verse_index + 1} }}}}{verse}"))
             else:
-                doc.append(NoEscape(f' {verse}'))
+                doc.append(NoEscape(f'{verse}'))
             doc.append(NewLine())
-        doc.append(NoEscape(r'\end{multicols}'))  # End 2-column layout
+
+        
+doc.append(NoEscape(r'\end{multicols}'))  # End 2-column layout
 
 
 
