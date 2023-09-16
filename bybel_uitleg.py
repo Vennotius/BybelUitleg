990ad94bca0280e79f8e2684c63966a6b4dc1ef4
Vennotius <mcadendorff@gmail.com>
2023-09-16 14:49:24 +0200

    goeie begin, maar nog werk is nodig

--- bybel_uitleg.py
+++ bybel_uitleg.py
@@ -1,4 +1,5 @@
 import json
+import subprocess
 
 BYBEL_BOEKNAME = [
     "Génesis",
@@ -75,47 +76,61 @@ with open('Bybel.json', 'r') as f:
     BYBEL_TEKS = json.loads(f.read())
 
 
-
-from pylatex import Document, Section, Command, NewPage, NewLine
+from pylatex import Document, Command, NewPage, NewLine, UnsafeCommand
 from pylatex.utils import NoEscape
+from pylatex.package import Package
 
 doc = Document()
+
+# Add the utf8x option for inputenc
+#doc.preamble.append(Package('inputenc', options='utf8x'))
+
+
+# Add packages for multicol and fancyhdr
+doc.preamble.append(Package('multicol'))
+doc.preamble.append(Package('fancyhdr'))
+
+# Set custom header and footer
+doc.preamble.append(NoEscape(r'\pagestyle{fancy}'))
+doc.preamble.append(NoEscape(r'\fancyhf{}'))  # Clear existing header and footer
+doc.preamble.append(NoEscape(r'\fancyhead[C]{\textbf{\rightmark}}'))  # Book name and chapter in header
+doc.preamble.append(NoEscape(r'\fancyfoot[C]{\thepage}'))  # Page number in footer
+
+# Title logic as before
 doc.preamble.append(Command('title', 'The Bible'))
 doc.preamble.append(Command('author', 'Anonymous'))
 doc.append(NoEscape(r'\maketitle'))
 
 
+# from unidecode import unidecode
 
-def add_bible_text(doc, bible_data):
-    for book in bible_data:
-        with doc.create(Section(book['book'], numbering=False)):
-            for chapter_index, chapter in enumerate(book['chapters'], start=1):
-                doc.append(f"Chapter {chapter_index}")
-                doc.append(NewLine())
-                
-                for verse_index, verse in enumerate(chapter, start=1):
-                    doc.append(f"{verse_index}. {verse}")
-                    doc.append(NewLine())
-
-# Example Bible data
-sample_data = [
-    {
-        'book': 'Genesis',
-        'chapters': [
-            ['In the beginning God created the heavens and the earth.', 'And the earth was formless and void.'],
-            ['Thus the heavens and the earth were completed.', 'And by the seventh day God completed His work.']
-        ]
-    },
-    {
-        'book': 'Exodus',
-        'chapters': [
-            ['Now these are the names of the sons of Israel.', 'Reuben, Simeon, Levi and Judah;'],
-            ['Issachar, Zebulun and Benjamin;', 'Dan and Naphtali, Gad and Asher.']
-        ]
-    }
-]
-
-add_bible_text(doc, sample_data)
+for book_index, book in enumerate(BYBEL_TEKS):
+    if book_index > 0:
+        break
+    for chapter_index, chapter in enumerate(book):
+        doc.append(NoEscape(r'\begin{multicols}{2}'))  # Start 2-column layout
+        doc.append(NoEscape(f'\\renewcommand{{\\rightmark}}{{{BYBEL_BOEKNAME[book_index]} Chapter {chapter_index + 1}}}'))  # Set header
+        doc.append(NoEscape(f'\\noindent\\fontsize{{24}}{{24}}\\selectfont\\textbf{{{chapter_index}}}\\normalsize'))
+        for verse_index, verse in enumerate(chapter):
+            if verse_index > 0:
+                doc.append(NoEscape(f'\\textsuperscript{{\\textbf{{{verse_index + 1}}}}} {verse}'))
+            else:
+                doc.append(NoEscape(f' {verse}'))
+            doc.append(NewLine())
+        doc.append(NoEscape(r'\end{multicols}'))  # End 2-column layout
 
 
-doc.generate_pdf("bible", clean_tex=True)
+
+
+
+import traceback
+
+try:
+    doc.generate_pdf("bible", clean_tex=True)
+except subprocess.CalledProcessError as e:
+    print("Error occurred while generating PDF.")
+    print("Command:", e.cmd)
+    print("Return code:", e.returncode)
+    print("Captured Output:", e.output)
+    traceback.print_exc()
+
